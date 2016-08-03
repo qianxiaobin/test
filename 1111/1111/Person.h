c3a46a6fdee394f439e1c4bbfc1a651e791a3314
qianxiaobin <qxb@qxbdemacbook-air.local>
2016-08-03 14:59:56 +0800

    Person

---
+++ 1111/1111/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  1111
+//
+//  Created by qxb on 16/8/3.
+//  Copyright © 2016年 qxb. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@end
---
+++ 1111/1111/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  1111
+//
+//  Created by qxb on 16/8/3.
+//  Copyright © 2016年 qxb. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
